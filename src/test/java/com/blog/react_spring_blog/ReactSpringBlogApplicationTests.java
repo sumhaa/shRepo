4e21dc6cba189715ae18ee68e337d03600655891
goott6 <crystalteacher@outlook.kr>
2025-01-02 09:57:38 +0900

    Commit

---
+++ src/test/java/com/blog/react_spring_blog/ReactSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.react_spring_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
